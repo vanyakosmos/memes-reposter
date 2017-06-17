0cb6f0933457a027eddde47757f52a419ccf0314
Bachynin Ivan <bachynin.i@gmail.com>
2017-06-17 20:36:58 +0300

    📚 document functions with google style

--- src/scheduler.py
+++ src/scheduler.py
@@ -11,12 +11,31 @@ logger = logging.getLogger('⌛ ' + __name__)
 
 
 def scheduling(job_queue: JobQueue, db: AbstractDB):
+    """
+    Scheduling for one run `getting posts job` 
+    and for repetitive running `db cleanup job`.
+    
+    Args:
+        job_queue (JobQueue): 
+        db (AbstractDB): 
+    """
     logger.info('Setting up schedule...')
     job_queue.run_once(get_posts_job, when=0, context=db)
     job_queue.run_repeating(cleanup_db_job, first=0, interval=CLEARING_DB_INTERVAL, context=db)
 
 
 def get_posts_job(_, job: Job):
+    """
+    Trying to obtain posts from Imgur. 
+    If successfully then filter all posts and schedule `posting job`.
+    Otherwise schedule `this` job to run after ``IMGUR_CHECK_INTERVAL`` seconds.
+    If after filtration remains 0 posts then also schedule `this` job.
+    
+    Args:
+        _ (telegram.Bot): Unused ``telegram.Bot`` instance required because ``get_posts_job(...)``
+            is using as callback in ``job_queue``.
+        job (job): Job object that holds ``job_queue`` and context parameter with database.
+    """
     logger.info('▶︎ Running 🌚 GET_POSTS job...')
     db = job.context
     job_queue = job.job_queue
@@ -41,6 +60,16 @@ def get_posts_job(_, job: Job):
 
 
 def posting_job(bot, job: Job):
+    """
+    If obtained not empty list with posts from job context 
+    then publish first post, remove it from posts list and schedule next `posting job`.
+    Otherwise schedule `get posts job`.
+    
+    Args:
+        bot (telegram.Bot): Bot instance needed for publishing post.
+        job (Job): Job object that holds ``job_queue`` and context parameter 
+            with database and list of posts.
+    """
     logger.info('▶︎ Running 📨 POSTING job...')
     posts, db = job.context
     job_queue = job.job_queue
@@ -57,6 +86,13 @@ def posting_job(bot, job: Job):
 
 
 def cleanup_db_job(_, job: Job):
+    """
+    Removing posts from the database that are older than ``CLEARING_DB_INTERVAL``.
+    
+    Args:
+        _ (telegram.Bot): Unused bot instance needed as part of callback signature.
+        job (Job): Job object that holds context parameter with database.
+    """
     db = job.context
     logger.info('▶︎ Running 🔥 CLEANUP_DATABASE job...')
     deleted, remaining = db.clear(CLEARING_DB_INTERVAL)
