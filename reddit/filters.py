94fd73166f4c7b38222aa9c0b283cb38298bf98b
Bachynin Ivan <bachynin.i@gmail.com>
2017-09-17 19:52:18 +0300

    🐎 db calls optimisation

--- reddit/filters.py
+++ reddit/filters.py
@@ -5,33 +5,40 @@ from core.store import IdStore
 from .models import Post
 
 
-class ScoreFilter(BaseFilter):
-    def __init__(self, limits):
-        super().__init__()
-        self.limits = limits
-
-    def filter(self, subreddits: List[List[Post]], *args, **kwargs) -> List[Post]:
-        self.logger.debug(f'Was: {sum([len(s) for s in subreddits])}')
-        posts = []
-
-        for i, subreddit in enumerate(subreddits):
-            filtered = [post for post in subreddit if post.score > self.limits[i][1]]
-            posts.extend(filtered)
-        self.logger.debug(f'Now: {len(posts)}')
-        return posts
-
-
+# before modelling
 class UniqueFilter(BaseFilter):
     def __init__(self, store: IdStore):
         super().__init__()
         self.store = store
 
     def filter(self, subreddits: List[List[dict]], *args, **kwargs):
-        self.logger.debug(f'Was: {sum([len(s) for s in subreddits])}')
         filtered_subs = []
-
         for posts in subreddits:
-            posts = [post for post in posts if not self.store.has_id(post['data']['id'])]
+            ids = [post['data']['id'] for post in posts]
+            posted = self.store.has_ids(ids)
+            posts = [posts[i] for i in range(len(ids)) if not posted[i]]
             filtered_subs.append(posts)
-        self.logger.debug(f'Now: {sum([len(s) for s in filtered_subs])}')
+
+        self.logger.debug(f'> {sum([len(s) for s in subreddits])}')
+        self.logger.debug(f'< {sum([len(s) for s in filtered_subs])}')
         return filtered_subs
+
+
+# after modelling
+class ScoreFilter(BaseFilter):
+    def __init__(self, limits):
+        super().__init__()
+        self.limits = limits
+
+    def filter(self, subreddits: List[List[Post]], *args, **kwargs) -> List[Post]:
+        posts = []
+        for i, subreddit in enumerate(subreddits):
+            filtered = [post for post in subreddit if post.score > self.limits[i][1]]
+            posts.extend(filtered)
+
+        self.logger.debug(f'> {sum([len(s) for s in subreddits])}')
+        self.logger.debug(f'< {len(posts)}')
+        return posts
+
+
+
