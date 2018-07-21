82d7c132847539789999fc5568c247831b506423
Bachynin Ivan <bachynin.i@gmail.com>
2018-07-21 16:24:58 +0300

    📝 fetch number of workers from nv

--- memes_reposter/settings.py
+++ memes_reposter/settings.py
@@ -145,7 +145,8 @@ TELEGRAM_TIMEOUT = 30  # seconds
 REDIS_URL = easy_env.get('REDIS_URL', 'redis://localhost:6379/0')
 CELERY_BROKER_URL = REDIS_URL
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_WORKER_CONCURRENCY = 1 if DEBUG else multiprocessing.cpu_count()
+CELERY_WORKER_CONCURRENCY = easy_env.get_int('CELERY_WORKER_CONCURRENCY',
+                                             default=multiprocessing.cpu_count())
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 # core
