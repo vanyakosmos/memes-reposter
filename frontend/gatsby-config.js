9c7ce138a661d53e24974da8d4d28939bef3c8f6
Bachynin Ivan <bachynin.i@gmail.com>
2019-08-15 00:17:31 +0300

    fix dark theme

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -20,10 +20,10 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        short_name: `reposter`,
+        start_url: `/reddit`,
+        background_color: `#1b1f21`,
+        theme_color: `#1b1f21`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,15 +1,30 @@
-import { deepOrange, indigo, red } from "@material-ui/core/colors"
+import { red } from "@material-ui/core/colors"
 import { createMuiTheme } from "@material-ui/core/styles"
 
 const theme = createMuiTheme({
     palette: {
-      type: "dark",
-      primary: indigo,
-      secondary: deepOrange,
-      error: red,
+      text: {
+        primary: '#e8e8e8',
+        secondary: '#d9d9d9',
+        disabled: '#aeaeae',
+        hint: '#e8e8e8',
+      },
       background: {
         default: "#272c2f",
+        paper: "#353c40",
       },
+      divider: '#e8e8e8',
+      action: {
+        active: '#e8e8e8',
+      },
+      primary: {
+        main: '#1b1f21',
+        contrastText: '#e8e8e8',
+      },
+      secondary: {
+        main: '#27a5ff',
+      },
+      error: red,
     },
     shape: {
       borderRadius: 10,
